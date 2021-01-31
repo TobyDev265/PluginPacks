07f25a6604ee74857c90aae69029f290bb91a4db
TobyDev265 <tobydev265@gmail.com>
2021-01-31 20:12:27 +0700

    Upload files

---
+++ src/TobyDev265/PluginPacks/Main.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace TobyDev265\PluginPacks;
+
+use pocketmine\{Server, Player};
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\{Command, CommandSender, ConsoleCommandSender};
+use pocketmine\utils\TextFormat as C;
+
+class Main extends PluginBase {
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool {
+        if($cmd->getName() == "pluginpacks") {
+            if(!$sender instanceof Player) {
+                if(empty($args[0]) || $args == null) {
+                    $this->unknownCmd($sender);
+                } else {
+                switch($args[0]) {
+                    case "list":
+                        $sender->sendMessage("Prison <1>\nSkyblock <2>");
+                        break;
+                    case "install":
+                        switch($args[1]) {
+                            case 1:
+                                // Prison
+                                // Include: Prisons, PurePerms, PureChat, EconomyAPI, MineReset, ScoreHud
+                                $plugin = ["Prisons", "PurePerms", "PureChat", "EconomyAPI", "MineReset", "ScoreHud"];
+                                foreach($plugin as $p) {
+                                    $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "install " . $p);
+                                }
+                                break;
+                            case 2:
+                                // Skyblock
+                                // Include: MultiWorld, SkyBlock, EconomyAPI, FormAPI, CustomShopUI, ScoreHud, EC-TableUI
+                                $plugin = ["MultiWorld", "SkyBlock", "FormAPI", "EconomyAPI", "CustomShopUI", "ScoreHud", "EC-TableUI"];
+                                foreach($plugin as $p) {
+                                    $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "install " . $p);
+                                }
+                                break;
+                            default:
+                                $sender->sendMessage(C::RED . "Cannot find plugin pack!");
+                                break;
+                        }
+                        break;
+                    case "remove":
+                        switch($args[1]) {
+                            case 1:
+                                // Prison
+                                // Include: Prisons, PurePerms, PureChat, EconomyAPI, MineReset, ScoreHud
+                                $plugin = ["Prisons", "PurePerms", "PureChat", "EconomyAPI", "MineReset", "ScoreHud"];
+                                foreach($plugin as $p) {
+                                    $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "uninstall " . $p);
+                                }
+                                break;
+                            case 2:
+                                // Skyblock
+                                // Include: MultiWorld, SkyBlock, EconomyAPI, FormAPI, CustomShopUI, ScoreHud, EC-TableUI
+                                $plugin = ["MultiWorld", "SkyBlock", "FormAPI", "EconomyAPI", "CustomShopUI", "ScoreHud", "EC-TableUI"];
+                                foreach($plugin as $p) {
+                                    $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "uninstall " . $p);
+                                }
+                                break;
+                            default:
+                                $sender->sendMessage(C::RED . "Cannot find plugin pack!");
+                                break;
+                        }
+                        break;
+                    case "help":
+                        $sender->sendMessage("Usage:\n pluginpacks install <pack> to install pack\n pluinpacks remove <pack> to remove pack\n pluginpacks list to see available packs");
+                        break;
+                    default:
+                        $this->unknownCmd($sender);
+                        break;
+                }
+            }
+            }
+        }
+        return 0;
+    }
+    public function unknownCmd($sender) {
+        $sender->sendMessage(C::RED . "Please type: /pluginpacks help");
+    }
+}
